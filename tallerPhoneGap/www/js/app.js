3268a204d1ec529ca6b47020159d3445c3d37647
Sergio Infante <rsinfante@gmail.com>
2015-10-26 10:41:37 -0500

    Implemented View Routing

--- tallerPhoneGap/www/js/app.js
+++ tallerPhoneGap/www/js/app.js
@@ -4,17 +4,26 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     
     var service = new EmployeeService();
     service.initialize().done(function () {
-        $('body').html(new HomeView(service).render().$el);
+        router.addRoute('', function() {
+            console.log('empty');
+            $('body').html(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('employees/:id', function(id) {
+            console.log('details');
+            service.findById(parseInt(id)).done(function(employee) {
+                $('body').html(new EmployeeView(employee).render().$el);
+            });
+        });
+
+        router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Directorio de Empleados v3.4");
-    });
     document.addEventListener('deviceready', function () {
         StatusBar.overlaysWebView( false );
         StatusBar.backgroundColorByHexString('#ffffff');
